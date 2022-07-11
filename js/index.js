fbadac3d36dd46a0ab80321bb2397d201f743307
labanshiks <labanshiks4@gmail.com>
2022-07-11 20:43:41 +0300

    daily updates 10

--- js/index.js
+++ js/index.js
@@ -10,23 +10,3 @@ navLinks.forEach((link) => {
     document.body.classList.remove("nav-open");
   });
 });
-
-
-
-const parentContainer =  document.querySelector('.about-me');
-
-parentContainer.addEventListener('click', event=>{
-
-    const current = event.target;
-
-    const isReadMoreBtn = current.className.includes('read-more-btn');
-
-    if(!isReadMoreBtn) return;
-
-    const currentText = event.target.parentNode.querySelector('.read-more-text');
-
-    currentText.classList.toggle('read-more-text--show');
-
-    current.textContent = current.textContent.includes('Read More') ? "Read Less..." : "Read More...";
-
-})
