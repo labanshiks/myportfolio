027f7dcc1520af79c796606bbe6cd4830f283781
labanshiks <labanshiks4@gmail.com>
2022-07-29 23:45:06 +0300

    daily updates 25

--- assets/js/index.js
+++ assets/js/index.js
@@ -70,7 +70,7 @@ ScrollReveal({
 
 ScrollReveal().reveal('.title__intro, .title__services, .title__about, .title__work, .title__contact, .service-list__item, .footer__link', { delay: 600, origin: "bottom" });
 ScrollReveal().reveal('.subtitle__intro, .subtitle__about, .subtitle__work, .services, .buttons, .submit-btn', { delay: 700, origin: "left" });
-ScrollReveal().reveal('.intro__img, .about-me__img', { delay: 800, origin: "top" });
+ScrollReveal().reveal('.intro__img, .about-me__img, .example__img', { delay: 800, origin: "top" });
 ScrollReveal().reveal('.about-me__body, .portfolio-about__body, .portfolio', { delay: 700, origin: "left" });
 ScrollReveal().reveal('.service-list__item, .social-list__link', { delay: 600, origin: "bottom", interval: 200 });
 
